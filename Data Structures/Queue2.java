4d957b07dfe70f404a6dead835b8716a75c875fe
SMU\201611033 <yejun_1220@naver.com>
2021-07-31 21:56:38 +0900

    Data Structure
    스택, https://www.acmicpc.net/problem/10828
    큐 2, https://www.acmicpc.net/problem/18258

---
+++ Data Structures/Queue2.java
@@ -0,0 +1,64 @@
+// https://www.acmicpc.net/problem/18258
+
+import java.io.*;
+import java.util.*;
+
+public class Queue2 {
+
+    public static void main(String[] args) throws IOException {
+        Deque<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<num; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            switch(st.nextToken()) {
+
+                case "push":
+                    queue.add(Integer.parseInt(st.nextToken()));
+                    break;
+
+                case "pop":
+                    if(!queue.isEmpty())
+                        bw.write(String.valueOf(queue.poll()) +"\n");
+                    else
+                        bw.write(-1 + "\n");
+                    break;
+
+                case "size":
+                    bw.write(String.valueOf(queue.size()) +"\n");
+                    break;
+
+                case "empty":
+                    if(!queue.isEmpty())
+                        bw.write(0 + "\n");
+                    else
+                        bw.write(1 + "\n");
+                    break;
+
+                case "front":
+                    if(!queue.isEmpty())
+                        bw.write(String.valueOf(queue.peek()) +"\n");
+                    else
+                        bw.write(-1 + "\n");
+                    break;
+
+                case "back":
+                    if(!queue.isEmpty())
+                        bw.write(String.valueOf(queue.peekLast()) +"\n");
+                    else
+                        bw.write(-1 + "\n");
+                    break;
+
+            }
+
+        }
+        bw.flush(); // 출력
+        br.close();
+        bw.close();
+    }
+}
