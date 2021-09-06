552db1904fd2be984509157aad9980f533177f80
yejun_1220@naver.com <yejun_1220@naver.com>
2021-09-06 23:41:15 +0900

    Data Structure
    괄호, https://www.acmicpc.net/problem/9012

--- Data Structures/Parenthesis.java
+++ Data Structures/Parenthesis.java
@@ -1,21 +1,19 @@
 // https://www.acmicpc.net/problem/9012
 
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
 class Parenthesis {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Stack<String> stack = new Stack<>(); // 자바SE7 이후부터 generic 생략 가능
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // System.in은 콘솔(터미널 등)로부터 입력(키보드) 받는 것을 의미한다.
 
-        Scanner sc = new Scanner(System.in); // System.in은 콘솔(터미널 등)로부터 입력(키보드) 받는 것을 의미한다.
-
-        int count = sc.nextInt(); // 숫자 입력 받는다.
-        String temp = sc.nextLine(); // 개행문자 '\n'을 저장한다.
+        int count = Integer.parseInt(br.readLine()); // 숫자 입력 받는다.
 
         for(int i=0; i<count; i++){
             boolean isError = false; // 에러 유무 확인 변수
 
-            String str = sc.nextLine(); // VPS 입력 받는다.
+            String str = br.readLine(); // VPS 입력 받는다.
 
             for(int j=0; j<str.length(); j++) {
                 char c = str.charAt(j);
@@ -45,7 +43,7 @@ class Parenthesis {
             stack.clear();
         }
 
-        sc.close();
+        br.close();
     }
 }
 
